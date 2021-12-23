24b12cfd38439426ccbd36d89779fcdc7fa335cc
Michael Gunkoff <painter@3lines.club>
2021-12-23 23:38:39 +0300

    more tests

--- models_test.go
+++ models_test.go
@@ -70,3 +70,78 @@ func TestDate_Format(t *testing.T) {
 		})
 	}
 }
+
+func TestDate_NextDay(t *testing.T) {
+	tests := []struct {
+		this Date
+		want Date
+	}{
+		{NewDate(2021, 12, 31), NewDate(2022, 1, 1)},
+		{NewDate(2021, 12, 1), NewDate(2021, 12, 2)},
+		{NewDate(2021, 2, 28), NewDate(2021, 3, 1)},
+	}
+	for _, tt := range tests {
+		t.Run(tt.this.String(), func(t *testing.T) {
+			if got := tt.this.NextDay(); got != tt.want {
+				t.Errorf("NextDay() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDate_NextWeek(t *testing.T) {
+	tests := []struct {
+		this Date
+		want Date
+	}{
+		{NewDate(2021, 12, 31), NewDate(2022, 1, 7)},
+		{NewDate(2021, 12, 1), NewDate(2021, 12, 8)},
+		{NewDate(2021, 2, 28), NewDate(2021, 3, 7)},
+	}
+	for _, tt := range tests {
+		t.Run(tt.this.String(), func(t *testing.T) {
+			if got := tt.this.NextWeek(); got != tt.want {
+				t.Errorf("NextDay() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDate_PreviousWeek(t *testing.T) {
+	tests := []struct {
+		this Date
+		want Date
+	}{
+		{NewDate(2021, 12, 31), NewDate(2021, 12, 24)},
+		{NewDate(2022, 1, 3), NewDate(2021, 12, 27)},
+		{NewDate(2021, 12, 1), NewDate(2021, 11, 24)},
+		{NewDate(2021, 3, 2), NewDate(2021, 2, 23)},
+	}
+	for _, tt := range tests {
+		t.Run(tt.this.String(), func(t *testing.T) {
+			if got := tt.this.PreviousWeek(); got != tt.want {
+				t.Errorf("NextDay() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDate_PreviousMonth(t *testing.T) {
+	tests := []struct {
+		this Date
+		want Date
+	}{
+		{NewDate(2021, 12, 31), NewDate(2021, 12, 1)},
+		{NewDate(2024, 2, 29), NewDate(2024, 1, 29)},
+		{NewDate(2021, 12, 1), NewDate(2021, 11, 1)},
+		{NewDate(2021, 3, 2), NewDate(2021, 2, 2)},
+		{NewDate(2024, 1, 1), NewDate(2023, 12, 1)},
+	}
+	for _, tt := range tests {
+		t.Run(tt.this.String(), func(t *testing.T) {
+			if got := tt.this.PreviousMonth(); got != tt.want {
+				t.Errorf("NextDay() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
